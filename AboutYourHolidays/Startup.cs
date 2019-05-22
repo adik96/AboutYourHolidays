b7fe5005ee1d285522f87a6f2c5ffa9fcaa34a10
tomciodsad <tomcio5346@gmail.com>
2019-05-22 19:01:18 +0200

    Add project files.

---
+++ AboutYourHolidays/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AboutYourHolidays.Startup))]
+namespace AboutYourHolidays
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
